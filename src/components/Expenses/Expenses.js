a8ddf37a5a62181f2d1fb8b21a81f89c3ad5f409
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-04-21 13:23:37 +0600

    Rendering list of data and outputting conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,24 @@
-import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
-import Card from '../UI/Card';
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import ExpensesFilter from "../Filter/ExpensesFilter";
+import Card from "../UI/Card";
+import { useState } from "react";
 
-const Expenses= (props) => {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const changeDataHandler = (filterData) => {
+    setFilteredYear(filterData);
+  };
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
   return (
     <Card className="expenses">
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onDataChange={changeDataHandler}
+      />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,38 @@
-import React from "react";
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import React, { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
-return <div className="new-expense">
-    <ExpenseForm />
-</div>
-}
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setAddExpense(false);
+  };
+  const [addExpense, setAddExpense] = useState(false);
+
+  const showExpenseForm = () => {
+    setAddExpense(true);
+  };
+  const closeExpenseForm = () => {
+    setAddExpense(false);
+  };
+  return (
+    <div className="new-expense">
+      {!addExpense ? (
+        <button type="button" onClick={showExpenseForm}>
+          Add New Expense
+        </button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCloseExpenseForm={closeExpenseForm}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
